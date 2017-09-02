028687c20f7bc2528ff669db35a3364d8484c002
root <rinzler.d.vicky@gmail.com>
2017-09-02 18:53:35 +0530

    Minor Modifications

---
+++ environments/cms/plugins/maple/class-admin.php
@@ -0,0 +1,18 @@
+<?php
+namespace maple\cms\plugin;
+/**
+ * Admin Content Handler
+ * @since 1.0
+ * @package Maple CMS
+ * @subpackage Maple CMS Plugin
+ * @author Rubixcode
+ */
+class ADMIN{
+
+	public static function get_dashboard_menu(){
+		var_dump(\maple\cms\MAPLE::debug()); die();
+		return [];
+	}
+}
+
+?>
